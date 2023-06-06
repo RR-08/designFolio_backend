2543e711b170eb7bf47070cbb5efcb80b11327d4
Riya <triya1315@gmail.com>
2023-06-06 19:24:19 +0530

    schemas

--- sanity.config.js
+++ sanity.config.js
@@ -1,8 +1,7 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
-
+import schemas from './schemas/index'
 export default defineConfig({
   name: 'default',
   title: 'designFolio_rr08',
@@ -13,6 +12,6 @@ export default defineConfig({
   plugins: [deskTool(), visionTool()],
 
   schema: {
-    types: schemaTypes,
+    types: schemas,
   },
 })
